33bdc39a95a412d128e4595903d99417b34c1ab8
Denis Koksal-Rivet <deniskoksalrivet@gmail.com>
2018-11-11 15:36:21 +0000

    Edited so that the value of pi_t passed into parameter_set_update is updated with every loop iteration

--- main.m
+++ main.m
@@ -72,19 +72,25 @@ R = 1;
 plot(x_t(1),x_t(2),'o','MarkerSize',5)
 hold on 
 theta = sdpvar(3,1);
+x = sdpvar(2,1);
 
 vertices = compute_vertices(PI_theta,pi_t);
-for i = 1:10
+for i = 1:500
+    i
     % Do the parameter set update with this function to get pi_t_plus_one
     % gives infeasible or unbounded model after several iterations
     if i ~= 1
         pi_t_plus_one = parameter_set_update(A0,A1,A2,A3,B0,B1,B2,B3,x_t_1,optimal_control_input,x_t,PI_theta,PI_w,pi_t,pi_w);
         % calculate vertices of the newly updated parameter set:
         vertices = compute_vertices(PI_theta,(pi_t_plus_one)');
+        % update the value of pi_t
+        pi_t = pi_t_plus_one';
     end
     
-    
     vertices
+%     if i ~= 1
+%         pi_t_plus_one
+%     end
     % update lambda_t:
     % lambda_t = update_lambda_t(vertices, H_hat);
     % disp(lambda_t)
@@ -92,7 +98,7 @@ for i = 1:10
     % compute the optimal solution:
     theta_hat = [0 0 0];
     theta_hat_transpose = [ones(length(vertices(:,1)),1) vertices];
-    [optimal_cost, optimal_control_input] = compute_optimal_solution(A0, A1, A2, A3, B0, B1, B2, B3, N, H_c, G, theta_hat_transpose, H_hat, V, PI_w, pi_w, vertices, K, R, Q, x_t, theta_hat);
+    [optimal_cost, optimal_control_input, alpha_k_0] = compute_optimal_solution(A0, A1, A2, A3, B0, B1, B2, B3, N, H_c, G, theta_hat_transpose, H_hat, V, PI_w, pi_w, vertices, K, R, Q, x_t, theta_hat);
 
     % disp(H_hat)
     % disp(H_Q)
@@ -100,12 +106,13 @@ for i = 1:10
     % disp(K)
     % disp(optimal_control_input)
     % disp(optimal_cost)
+    % alpha_k_0
     
     % x_t_1 = x_t;
     x_t_1 = x_t;
     theta_used_to_update_state = [0 0 0];
     [A_theta, B_theta] = calculate_AandB_theta_j(B0,B1,B2,B3,A0,A1,A2,A3,theta_used_to_update_state);
-    x_t = A_theta * x_t + B_theta * optimal_control_input + [randi([0 1000])/10000;randi([0 1000])/10000];
+    x_t = A_theta * x_t + B_theta * optimal_control_input + [(randi([0 2000])-1000)/10000;(randi([0 2000])-1000)/10000];
     
     plot(x_t(1),x_t(2),'o','MarkerSize',5)
 end
