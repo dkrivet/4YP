25a1d86c73a99ffd602fdd488c3ce3fe11eefe15
Denis Koksal-Rivet <deniskoksalrivet@gmail.com>
2018-11-15 20:05:05 +0000

    minor changes

--- point_estimate.m
+++ point_estimate.m
@@ -1,13 +1,10 @@
-function current_point_estimate = point_estimate(A0, A1, A2, A3, B0, B1, B2, B3, x_k, x_k_1, u_k, u_k_1, previous_point_estimate, PI_theta, pi_theta)
+function current_point_estimate = point_estimate(A0, A1, A2, A3, B0, B1, B2, B3, x_k, x_k_1, u_k, u_k_1, previous_point_estimate, PI_theta, pi_theta, mu)
 
 D_x_u = compute_D_of_x_and_u(A1, A2, A3, B1, B2, B3, x_k, u_k);
 D_x_1_u_1 = compute_D_of_x_and_u(A1, A2, A3, B1, B2, B3, x_k_1, u_k_1);
 
 [A_theta_hat_k_1, B_theta_hat_k_1] = calculate_AandB_theta_j(B0,B1,B2,B3,A0,A1,A2,A3,previous_point_estimate);
 x_hat_1_at_k_1 = A_theta_hat_k_1 * x_k_1 + B_theta_hat_k_1 * u_k_1;
-
-% arbitrarily set mu at the moment because I dont know how to calculate it
-mu = 1;
 
 theta_k_tilda = previous_point_estimate + mu * D_x_1_u_1' * (x_k - x_hat_1_at_k_1);
 
