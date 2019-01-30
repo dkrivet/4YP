02d0398f27ec98ed5b467874f6a6fcaa7f18214a
Denis Koksal-Rivet <deniskoksalrivet@gmail.com>
2019-01-30 15:45:22 +0000

    Changed to use multidimensional arrays rather than cell arrays

--- calculate_U_j.m
+++ calculate_U_j.m
@@ -1,12 +1,14 @@
 function U_j = calculate_U_j(R_j, V)
 
-for i = 1:length(R_j)
-    U_j{i} = [];
-end
+% for i = 1:length(R_j)
+%     U_j{i} = [];
+% end
+
+U_j = zeros(2, length(V(:,1)), length(V(:,1)));
 
 I = eye(length(V(:,1)));
 
 for i = 1:length(R_j)
-    U_j{i} = (V([R_j{i}(1,1), R_j{i}(1,2)],:))^(-1) * I([R_j{i}(1,1), R_j{i}(1,2)],:);
+    U_j(:,:,i) = (V([R_j(i,1), R_j(i,2)],:))^(-1) * I([R_j(i,1), R_j(i,2)],:);
 end 
 
