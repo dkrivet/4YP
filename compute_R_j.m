4565b03ed744804975e5344c7de5f769cdae4a97
Denis Koksal-Rivet <deniskoksalrivet@gmail.com>
2019-01-30 15:45:36 +0000

    Changed to use matrices rather than cell arrays

--- compute_R_j.m
+++ compute_R_j.m
@@ -2,17 +2,20 @@ function R_j = compute_R_j(V, tube_vertices, alpha)
 
 no_of_vertices = length(tube_vertices(:,1));
 
-for i = 1:no_of_vertices
-    R_j{i} = [];
-end 
+% for i = 1:no_of_vertices
+%     R_j{i} = [];
+% end 
+R_j = zeros(no_of_vertices,2);
 
 
 for j = 1:no_of_vertices
+    row_to_append = [];
     for r = 1:length(tube_vertices(:,1))
         if ((alpha(r) - 0.0001) < V(r,:) * tube_vertices(j,:)') && (alpha(r) + 0.0001 > V(r,:) * tube_vertices(j,:)')
-            R_j{j} = [R_j{j}, r];
+            row_to_append = [row_to_append, r];
         end
     end
+    R_j(j,:) = row_to_append;
 end 
 
 end
