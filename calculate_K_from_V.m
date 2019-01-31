81532093fcbb2aa880fd252fc7a20966c5bf436f
Denis Koksal-Rivet <deniskoksalrivet@gmail.com>
2019-01-31 09:21:31 +0000

    Replaced cell arrays with multidimensional arrays

--- calculate_K_from_V.m
+++ calculate_K_from_V.m
@@ -13,18 +13,21 @@ theta_hat_transpose = [ones(length(vertices(:,1)),1) vertices];
 lambda = sdpvar(1);
 K = sdpvar(1,2);
 
-H_hat = sdpvar((p + 1)*ones(1,n_alpha),n_alpha * ones(1,n_alpha));
+% H_hat = sdpvar((p + 1)*ones(1,n_alpha),n_alpha * ones(1,n_alpha));
+H_hat = sdpvar(p+1,n_alpha,n_alpha);
+
+
 
 % set constraints
 Constraints = [];
 for i = 1:n_alpha
     for j = 1: length(vertices(:,1))
-        Constraints = [Constraints, lambda >= theta_hat_transpose(j,:) * H_hat{i} * ones(size(V(:,1))), theta_hat_transpose(j,:) * H_hat{i} >= 0];
+        Constraints = [Constraints, lambda >= theta_hat_transpose(j,:) * H_hat(:,:,i) * ones(size(V(:,1))), theta_hat_transpose(j,:) * H_hat(:,:,i) >= 0];
     end
 end 
 
 for i = 1:n_alpha
-    Constraints = [Constraints, H_hat{i}*V==[V(i,:) * (A0 + B0 * K); V(i,:) * (A1 + B1 * K); V(i,:) * (A2 + B2 * K); V(i,:) * (A3 + B3 * K)]];
+    Constraints = [Constraints, H_hat(:,:,i)*V==[V(i,:) * (A0 + B0 * K); V(i,:) * (A1 + B1 * K); V(i,:) * (A2 + B2 * K); V(i,:) * (A3 + B3 * K)]];
 end
    
 % set objective
