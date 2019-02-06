9016c85537674abe1bb6ab4bca8d0df33d75161f
Denis Koksal-Rivet <deniskoksalrivet@gmail.com>
2019-02-06 15:34:11 +0000

    Change in horizon

--- calculate_M0.m
+++ calculate_M0.m
@@ -2,13 +2,13 @@ function M0 = calculate_M0(x, u, i, A1, A2, A3, B1, B2, B3)
 
 M0 = 0;
 
-if i < 10
+if i < 5
     for j = 1:i
         D_k = compute_D_of_x_and_u(A1, A2, A3, B1, B2, B3, x(:,j), u(j));
         M0 = M0 + D_k' * D_k; 
     end
 else
-    for j = i-9:i
+    for j = i-4:i
         D_k = compute_D_of_x_and_u(A1, A2, A3, B1, B2, B3, x(:,j), u(j));
         M0 = M0 + D_k' * D_k;        
     end
