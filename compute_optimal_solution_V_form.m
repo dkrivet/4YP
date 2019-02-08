3ec25ebe71d867528709ccaba311ca423d2303fe
Denis Koksal-Rivet <deniskoksalrivet@gmail.com>
2019-02-08 11:06:22 +0000

    Changed the PE condition to include future predicted inputs as well

--- compute_optimal_solution_V_form.m
+++ compute_optimal_solution_V_form.m
@@ -1,4 +1,4 @@
-function [optimal_cost, optimal_control_input, alpha_k_current, alpha_k_1, predicted_v, sol] = compute_optimal_solution_V_form(A0, A1, A2, A3, B0, B1, B2, B3, N, F, G, V, PI_w, pi_w, vertices, K, R, Q, x_k, number_of_vertices, U_j, PI_theta, pi_theta, theta_hat, M0, previous_control_input)
+function [optimal_cost, optimal_control_input, alpha_k_current, alpha_k_1, predicted_v, sol] = compute_optimal_solution_V_form(current_time_step, A0, A1, A2, A3, B0, B1, B2, B3, N, F, G, V, PI_w, pi_w, vertices, K, R, Q, x_k, number_of_vertices, U_j, PI_theta, pi_theta, theta_hat, M0, previous_control_input, previous_v)
 
 m = length(vertices(:,1));
 % use compute_w_bar to get the value of w_bar for use later on:
@@ -24,9 +24,12 @@ Lambda = sdpvar(length(V(:,1)),6,N+1,number_of_vertices,'full');
 [H, f_transpose] = construct_cost_matrices(A0, A1, A2, A3, B0, B1, B2, B3, K, N, R, Q, x_k, theta_hat);
 % define objective here ...
 lambda = 10^6;
-Objective = v_k' * H * v_k + 2 * f_transpose * v_k - lambda * beta;
-% Objective = v_k' * H * v_k + 2 * f_transpose * v_k;
-
+if current_time_step > 1
+    Objective = v_k' * H * v_k + 2 * f_transpose * v_k - lambda * beta;
+else
+    Objective = v_k' * H * v_k + 2 * f_transpose * v_k;
+end
+% Objective = v_k' * H * v_k + 2 * f_transpose * v_k - lambda * beta;  
 
 % define constraints:
 Constraints = [];
@@ -73,15 +76,45 @@ end
 
 
 
-D_x_u0 = compute_D_of_x_and_u(A1, A2, A3, B1, B2, B3, x_k, previous_control_input);
-L_du = compute_L_of_du(B1, B2, B3, du);
+% D_x_u0 = compute_D_of_x_and_u(A1, A2, A3, B1, B2, B3, x_k, previous_control_input);
+% L_du = compute_L_of_du(B1, B2, B3, du);
+% 
+% % check this constraint is satisfied 
+% % Constraint type: Matrix inequality
+% Constraints = [Constraints, M0 + D_x_u0' * D_x_u0 + D_x_u0' * L_du + L_du' * D_x_u0 >= beta];
+% % Constraint type: Element-wise inequality
+% Constraints = [Constraints, beta >= 0];
 
-% check this constraint is satisfied 
-% Constraint type: Matrix inequality
-Constraints = [Constraints, M0 + D_x_u0' * D_x_u0 + D_x_u0' * L_du + L_du' * D_x_u0 >= beta];
-% Constraint type: Element-wise inequality
-Constraints = [Constraints, beta >= 0];
 
+
+if current_time_step > 1
+    % New constraints PE into future:
+    [A_theta_hat, B_theta_hat] = calculate_AandB_theta_j(B0,B1,B2,B3,A0,A1,A2,A3,theta_hat);
+    for i = 1:(N-1)
+        v_hat(i) = previous_v(i+1);
+    end
+    
+    x_hat = zeros(2,N);
+    for i = 1:(N-1)
+        if i == 1
+            x_hat(:,i) = x_k;
+        end
+        u_hat(i) = K * x_hat(:,1) + v_hat(i);
+        x(:,i+1) = A_theta_hat * x_hat(:,i) + B_theta_hat * u_hat(i);
+    end
+    % v_hat, x_hat, and u_hat are now fully defined
+
+    for j = 1:length(U_j(:,:,1))    
+        sum = 0;
+        for i = 1:N-1
+            D_hat = compute_D_of_x_and_u(A1, A2, A3, B1, B2, B3, x_hat(:,i), u_hat(i));
+            D_tilda = compute_D_of_x_and_u(A1, A2, A3, B1, B2, B3, U_j(:,:,j)*alpha_k(:,i)-x_hat(i), u_hat(i));
+            sum = sum + D_hat'*D_hat + D_hat'*D_tilda + D_tilda'*D_hat;
+        end
+        Constraints = [Constraints, sum >= beta];
+    end
+end
+Constraints = [Constraints, beta>=0];
 
 % set solver options
 options = sdpsettings('solver','mosek','verbose',0, 'cachesolvers', 1);
