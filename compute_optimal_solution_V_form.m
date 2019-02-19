f9e458777b5476b80dfb55f3a1ed6b6b580d273b
Denis Koksal-Rivet <deniskoksalrivet@gmail.com>
2019-02-19 14:21:43 +0000

    Implementing the future PE condition at every other time step

--- compute_optimal_solution_V_form.m
+++ compute_optimal_solution_V_form.m
@@ -1,4 +1,4 @@
-function [optimal_cost, optimal_control_input, alpha_k_current, alpha_k_1, predicted_v, sol] = compute_optimal_solution_V_form(current_time_step, A0, A1, A2, A3, B0, B1, B2, B3, N, F, G, V, PI_w, pi_w, vertices, K, R, Q, x_k, number_of_vertices, U_j, PI_theta, pi_theta, theta_hat, M0, previous_control_input, previous_v)
+function [optimal_cost, optimal_control_input, alpha_k_current, alpha_k_to_return, predicted_v, sol] = compute_optimal_solution_V_form(current_time_step, A0, A1, A2, A3, B0, B1, B2, B3, N, F, G, V, PI_w, pi_w, vertices, K, R, Q, x_k, number_of_vertices, U_j, PI_theta, pi_theta, theta_hat, M0, previous_control_input, previous_v)
 
 m = length(vertices(:,1));
 % use compute_w_bar to get the value of w_bar for use later on:
@@ -23,12 +23,24 @@ Lambda = sdpvar(length(V(:,1)),6,N+1,number_of_vertices,'full');
 % value
 [H, f_transpose] = construct_cost_matrices(A0, A1, A2, A3, B0, B1, B2, B3, K, N, R, Q, x_k, theta_hat);
 % define objective here ...
-lambda = 10^6;
+% if current_time_step <= 30
+%     lambda = 10^6;
+% else
+%     lambda = 0;
+% end
+
+if mod(current_time_step,2) == 0
+    lambda = 10^6;
+else
+    lambda = 0;
+end
+
 if current_time_step > 1
     Objective = v_k' * H * v_k + 2 * f_transpose * v_k - lambda * beta;
 else
     Objective = v_k' * H * v_k + 2 * f_transpose * v_k;
 end
+% lambda = 10^4;
 % Objective = v_k' * H * v_k + 2 * f_transpose * v_k - lambda * beta;  
 
 % define constraints:
@@ -87,7 +99,8 @@ end
 
 
 
-if current_time_step > 1
+% if current_time_step > 1 && current_time_step <=30
+if current_time_step > 1 && mod(current_time_step,2) == 0
     % New constraints PE into future:
     [A_theta_hat, B_theta_hat] = calculate_AandB_theta_j(B0,B1,B2,B3,A0,A1,A2,A3,theta_hat);
     for i = 1:(N-1)
@@ -100,7 +113,7 @@ if current_time_step > 1
             x_hat(:,i) = x_k;
         end
         u_hat(i) = K * x_hat(:,1) + v_hat(i);
-        x(:,i+1) = A_theta_hat * x_hat(:,i) + B_theta_hat * u_hat(i);
+        x_hat(:,i+1) = A_theta_hat * x_hat(:,i) + B_theta_hat * u_hat(i);
     end
     % v_hat, x_hat, and u_hat are now fully defined
 
@@ -111,10 +124,12 @@ if current_time_step > 1
             D_tilda = compute_D_of_x_and_u(A1, A2, A3, B1, B2, B3, U_j(:,:,j)*alpha_k(:,i)-x_hat(i), u_hat(i));
             sum = sum + D_hat'*D_hat + D_hat'*D_tilda + D_tilda'*D_hat;
         end
-        Constraints = [Constraints, sum >= beta];
+        Constraints = [Constraints, M0 + sum >= beta];
     end
+    Constraints = [Constraints, beta>=0];
+    Constraints = [Constraints, beta<=100];
 end
-Constraints = [Constraints, beta>=0];
+
 
 % set solver options
 options = sdpsettings('solver','mosek','verbose',0, 'cachesolvers', 1);
@@ -126,8 +141,10 @@ sol = optimize(Constraints, Objective, options);
 optimal_cost = value(v_k' * H * v_k + 2 * f_transpose * v_k);
 optimal_control_input = value(K * x_k + v_k(1));
 alpha_k_current = value(alpha_k(:,1));
-alpha_k_1 = value(alpha_k(:,2));
+alpha_k_to_return = value(alpha_k);
 predicted_v = value(v_k);
+
+
 
 
 
